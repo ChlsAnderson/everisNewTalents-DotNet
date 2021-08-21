1ea52b9ba70e815ed02190f5102ddbf75ed9cf89
charles_mesquita <charles_mesquita@kawasakibrasil.com>
2021-08-21 19:14:52 -0300

    struct criada

---
+++ Revisao/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno[] alunos = new Aluno[5];
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        //TODO: adicionar aluno
+                        break;
+                    case "2":
+                        //TODO: listar alunos
+                        break;
+                    case "3":
+                        //TODO: Calcular media geral
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+
+                }
+
+                opcaoUsuario = ObterOpcaoUsuario();
+
+            }
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine("Informe a opção desejada:");
+            Console.WriteLine("1- Inserir novo aluno");
+            Console.WriteLine("2- Lista alunos");
+            Console.WriteLine("3- Calcular média geral");
+            Console.WriteLine("X- Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+            Console.WriteLine();
+            return opcaoUsuario;
+        }
+    }
+}
