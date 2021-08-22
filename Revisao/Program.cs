829e97b22acd2801b9ca760ae23a7822e302568c
charles_mesquita <charles_mesquita@kawasakibrasil.com>
2021-08-21 21:54:11 -0300

    implementado com valores enum para o conceito enum

--- Revisao/Program.cs
+++ Revisao/Program.cs
@@ -57,7 +57,32 @@ namespace Revisao
                         }
 
                         var mediaGeral = notaTotal / nrAlunos;
-                        Console.WriteLine($"MÉDIA GERAL: {mediaGeral}");
+                        Conceito conceitoGeral;
+
+                        if(mediaGeral < 2)
+                        {
+                            conceitoGeral = Conceito.E;
+                        }
+                        else if (mediaGeral < 4)
+                        {
+                            conceitoGeral = Conceito.D;
+                        }
+                        else if (mediaGeral < 6)
+                        {
+                            conceitoGeral = Conceito.C;
+                        }
+                        else if (mediaGeral < 8)
+                        {
+                            conceitoGeral = Conceito.B;
+                        }
+                        else
+                        {
+                            conceitoGeral = Conceito.A;
+                        }
+                        Console.WriteLine($"MÉDIA GERAL: {mediaGeral} - CONCEITO: {conceitoGeral}");
+                        Console.WriteLine();
+
+
                         break;
                     default:
                         throw new ArgumentOutOfRangeException();
@@ -71,6 +96,7 @@ namespace Revisao
 
         private static string ObterOpcaoUsuario()
         {
+            Console.WriteLine();
             Console.WriteLine("Informe a opção desejada:");
             Console.WriteLine("1- Inserir novo aluno");
             Console.WriteLine("2- Lista alunos");
