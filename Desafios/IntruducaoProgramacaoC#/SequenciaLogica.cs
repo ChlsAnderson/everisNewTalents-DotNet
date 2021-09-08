be2b08beaf4fd796075c12077e9059b10757c8bc
charles_mesquita <charles_mesquita@kawasakibrasil.com>
2021-09-07 21:34:55 -0300

    Desafios C# intrudocao programacao c#

---
+++ Desafios/IntruducaoProgramacaoC#/SequenciaLogica.cs
@@ -0,0 +1,16 @@
+using System; 
+
+class{
+
+    static void Main(string[] args) { 
+
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (                  )
+            {
+                Console.WriteLine(                 );
+                Console.WriteLine(                          );
+            }
+
+    }
+
+}
