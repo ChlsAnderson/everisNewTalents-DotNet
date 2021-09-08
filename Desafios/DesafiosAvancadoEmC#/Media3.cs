9ada4c7d167a25c6eb36ea53ac12dc7c6a4ecb77
charles_mesquita <charles_mesquita@kawasakibrasil.com>
2021-09-07 22:10:21 -0300

    Solucao Média 3

--- Desafios/DesafiosAvancadoEmC#/Media3.cs
+++ Desafios/DesafiosAvancadoEmC#/Media3.cs
@@ -1,47 +1,40 @@
 using System;
 
-namespace Start
-{
-    class Media3
-    {
-        static void Main(string[] args)
-        {
-            double a, b, c, d,avg;
-            string[] values = Console.ReadLine().Split(' ');
-            a = double.Parse(      );
-            b = double.Parse(     );
-            c = double.Parse(      );
-            d = double.Parse(      );
-            avg = (         );
-            Console.WriteLine("Media: " +avg.ToString("0.0"));
-            if (      )
-            {
-                Console.WriteLine("Aluno aprovado.");
-            }
-            if (       )
-            {
-                Console.WriteLine("Aluno reprovado.");
-            }
-            if (              )
-            {
-                Console.WriteLine("Aluno em exame.");
-                Console.Write("Nota do exame: ");
-                double e = double.Parse(Console.ReadLine());
-                avg = (             ) / 2;
-                if (                    )
-                {
-                    Console.WriteLine("Aluno aprovado.");
-                }
-                if (                  )
-                {
-                    Console.WriteLine("Aluno reprovado.");
-                }
-                Console.WriteLine("Media final: " +avg.ToString("0.0"));
-            }
-
-            Console.ReadKey();
+namespace Start {
+  class Media3 {
+    static void Main(string[] args) {
+      double a, b, c, d, avg;
+      string[] values = Console.ReadLine().Split(' ');
+      
+      a = double.Parse(values[0])*2;
+      b = double.Parse(values[1])*3;
+      c = double.Parse(values[2])*4;
+      d = double.Parse(values[3])*1;
+      
+      avg = (a + b + c + d) / 10;
+      Console.WriteLine("Media: " + avg.ToString("0.0"));
+      
+      if (avg >= 7) {
+        Console.WriteLine("Aluno aprovado.");
+      } else if (avg < 5) {
+        Console.WriteLine("Aluno reprovado.");
+      } else if (avg >= 5 && avg <= 6.9) {
+        double e = double.Parse(Console.ReadLine());
+        
+        Console.WriteLine("Aluno em exame.");
+        Console.WriteLine("Nota do exame: " + e.ToString("0.0"));
+        avg = (avg + e) / 2;
+        
+        if (avg >= 5) {
+          Console.WriteLine("Aluno aprovado.");
+        } else {
+          Console.WriteLine("Aluno reprovado");
         }
+        Console.WriteLine("Media final: " + avg.ToString("0.0"));
+      }
+      Console.ReadKey();
     }
+  }
 }
 
 /* 
