61e828a61be5ff51dcc9cb74fbd48c7789eb2cd2
charles_mesquita <charles_mesquita@kawasakibrasil.com>
2021-09-07 21:47:50 -0300

    Correção da solução media 2

--- Desafios/IntruducaoProgramacaoC#/Media2.cs
+++ Desafios/IntruducaoProgramacaoC#/Media2.cs
@@ -1,8 +1,7 @@
 using System;
 
-namespace Start
-{
-    class Program {
+namespace Start {
+  class Program {
     static void Main(string[] args) {
       double A, B, C;
       
@@ -13,5 +12,5 @@ namespace Start
       Console.WriteLine("MEDIA = " + String.Format("{0:0.0}", ((A * 2) + (B * 3) + (C * 5)) / (2 + 3 + 5)));
       Console.ReadKey();
     }
-    }
+  }
 }
