8613bbd0aec200d7ed3a24cd0a4e669f2b029f26
charles_mesquita <charles_mesquita@kawasakibrasil.com>
2021-09-07 21:50:28 -0300

    Desafio Positivo Medias

---
+++ Desafios/IntruducaoProgramacaoC#/PositivoMedias.cs
@@ -0,0 +1,52 @@
+using System; 
+
+class {
+
+        public static void Main()
+        {
+            double a, b = 0, avg;
+            int i, postive = 0;
+            for (                             )
+            {
+                
+                if (        )
+                {
+                   
+                }
+            }
+            avg = b / postive;
+            Console.WriteLine("{0} valores positivos", postive);
+            Console.WriteLine("{0}", avg.ToString("0.0"));
+
+            Console.ReadLine();
+        }
+
+}
+
+/* 
+ Básico
+ Princípios Básicos
+Leia 6 valores. Em seguida, mostre quantos destes valores digitados foram positivos. Na próxima linha, deve-se mostrar a média de todos os valores positivos digitados, com um dígito após o ponto decimal.
+
+Entrada
+A entrada contém 6 números que podem ser valores inteiros ou de ponto flutuante. Pelo menos um destes números será positivo.
+
+Saída
+O primeiro valor de saída é a quantidade de valores positivos. A próxima linha deve mostrar a média dos valores positivos digitados.
+
+ 
+Exemplo de Entrada	
+7
+-5
+6
+-3.4
+4.6
+12
+
+Exemplo de Saída
+4 valores positivos
+7.4
+
+
+
+ */
